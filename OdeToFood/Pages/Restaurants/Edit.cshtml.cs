c79c0637e9f1b85d059e3f559a62d7479663ea81
fhnribeiro <fhnribeiro@hotmail.com>
2021-04-12 23:39:33 -0300

    Edição de dados

---
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class EditModel : PageModel
+    {
+        private IRestaurantData restaurantData;
+        private IHtmlHelper htmlHelper;
+
+        public Restaurant Restaurant { get; set; }
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
+
+        public EditModel(IRestaurantData restaurantData,IHtmlHelper htmlHelper)
+        {
+            this.restaurantData = restaurantData;
+            this.htmlHelper = htmlHelper;
+        }
+
+        public IActionResult OnGet(int restaurantId)
+        {
+            Restaurant = restaurantData.GetByID(restaurantId);
+            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+            if(Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+    }
+}
